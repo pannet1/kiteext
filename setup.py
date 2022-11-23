c1ef700410e5589a990d7959998d7a4cc278b7f9
karthick <prog@ecomsense.in>
2022-11-23 13:30:52 +0530

    _ removed from pkgs

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup(name='kiteext-2022',
+setup(name='kiteext',
       version='1.0',
       description='a fork of kiteext',
       author='b karthick',
       author_email='prog@ecomsense.in',
       url='https://github.com/pannet1/kiteext/',
-      packages=['kiteext-2022']
+      packages=['kiteext']
       )
