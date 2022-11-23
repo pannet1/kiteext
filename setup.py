27b9ef13c5bb47913a8d32979de998901e3eb742
karthick <prog@ecomsense.in>
2022-11-23 12:42:58 +0530

    add requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='kiteext-2022',
+      version='1.0',
+      description='a fork of kiteext',
+      author='b karthick',
+      author_email='prog@ecomsense.in',
+      url='https://github.com/pannet1/kiteext-2022/',
+      packages=['kiteext-2022']
+      )
